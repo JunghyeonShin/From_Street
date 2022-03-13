f606cbd6fe4937f65ff2c2ee6c0e64e281931f27
Zziroo <tlslrs1994@naver.com>
2022-03-13 15:08:21 +0900

    Refactor : Refactoring Player Rotation 플레이어 회전 코드 리펙토링

--- FromStreet/Assets/Scripts/Player/PlayerRotation.cs
+++ FromStreet/Assets/Scripts/Player/PlayerRotation.cs
@@ -4,24 +4,17 @@ using UnityEngine;
 
 public class PlayerRotation : MonoBehaviour
 {
-    [SerializeField] private float _rotateSpeed = 0f;
+    [SerializeField] private float _rotateTime = 0f;
 
     private PlayerInput _playerInput = null;
 
-    private Transform _playerTransform = null;
-
-    private EPlayerMoveDirections _playerDir = EPlayerMoveDirections.Forward;
-
     private bool _isRotation = false;
-
-    private bool _sendCoroutineStartMessage = false;
 
     private float _angle = 0f;
 
     private void Start()
     {
         _playerInput = GetComponent<PlayerInput>();
-        _playerTransform = GetComponent<Transform>();
     }
 
     private void Update()
@@ -29,10 +22,7 @@ public class PlayerRotation : MonoBehaviour
         if (false == _isRotation)
         {
             CheckInputMessage();
-        }
 
-        if (_sendCoroutineStartMessage)
-        {
             StartCoroutine(Rotation(_angle));
         }
     }
@@ -41,50 +31,46 @@ public class PlayerRotation : MonoBehaviour
     {
         if (_playerInput.MoveForward)
         {
-            _playerDir = EPlayerMoveDirections.Forward;
             _angle = 0f;
             _isRotation = true;
-            _sendCoroutineStartMessage = true;
         }
         else if (_playerInput.MoveBack)
         {
-            _playerDir = EPlayerMoveDirections.Back;
             _angle = 180f;
             _isRotation = true;
-            _sendCoroutineStartMessage = true;
         }
         else if (_playerInput.MoveLeft)
         {
-            _playerDir = EPlayerMoveDirections.Left;
             _angle = -90f;
             _isRotation = true;
-            _sendCoroutineStartMessage = true;
         }
         else if (_playerInput.MoveRight)
         {
-            _playerDir = EPlayerMoveDirections.Right;
             _angle = 90f;
             _isRotation = true;
-            _sendCoroutineStartMessage = true;
         }
     }
 
     private IEnumerator Rotation(float angle)
     {
-        _sendCoroutineStartMessage = false;
+        float elapsedTime = 0f;
+
+        Quaternion start = transform.rotation;
+        Quaternion dest = Quaternion.Euler(0, angle, 0);
 
         while (_isRotation)
         {
-            _playerTransform.Rotate(new Vector3(0f, _rotateSpeed * Time.deltaTime, 0f));
+            elapsedTime += Time.deltaTime;
 
-            if (_playerTransform.localEulerAngles.y > angle)
+            if(elapsedTime >= _rotateTime)
             {
+                elapsedTime = _rotateTime;
                 _isRotation = false;
             }
 
+            transform.rotation = Quaternion.Slerp(start, dest, elapsedTime / _rotateTime);
+
             yield return null;
         }
-
-        Debug.Log($"{_playerTransform.localEulerAngles.y}");
     }
 }
