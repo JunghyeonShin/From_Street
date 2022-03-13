fc386a447b0aa6794284dbea3724ffe872d4ae67
Zziroo <tlslrs1994@naver.com>
2022-03-13 14:55:12 +0900

    Feat : Revise Camera Position 플레이어와의 거리 보정

--- FromStreet/Assets/Scripts/CameraMoving.cs
+++ FromStreet/Assets/Scripts/CameraMoving.cs
@@ -8,7 +8,33 @@ public class CameraMoving : MonoBehaviour
 
     [SerializeField] private float _moveSpeed = 0f;
 
+    [SerializeField] private float _reviseCameraPositionTime = 0f;
+
+    private enum ECameraDirections { None, Forward, Left, Right, }
+
+    private BezierCurve _bezierCurve = new BezierCurve();
+
     private Transform _cameraTransform = null;
+
+    private Vector3 _bezierStartPoint = new Vector3();
+    private Vector3 _bezierTempPoint = new Vector3();
+    private Vector3 _bezierEndPoint = new Vector3();
+
+    private ECameraDirections _cameraDirection = ECameraDirections.None;
+
+    private float _horizontalDistance = 0f;
+    private float _verticalDistance = 0f;
+
+    private bool _isChasingPlayer = false;
+
+    private readonly Vector3 _moveToForwardBetweenTwoPoints = new Vector3(0f, 0f, 1.25f);
+    private readonly Vector3 _moveToLeftBetweenTwoPoints = new Vector3(-1.25f, 0f, 0f);
+    private readonly Vector3 _moveToRightBetweenTwoPoints = new Vector3(1.25f, 0f, 0f);
+
+    private readonly Vector3 _nextForwardPoint = new Vector3(0f, 0f, 2f);
+    private readonly Vector3 _nextLeftPoint = new Vector3(-2f, 0f, 0f);
+    private readonly Vector3 _nextRightPoint = new Vector3(2f, 0f, 0f);
+
 
     private void Start()
     {
@@ -17,17 +43,66 @@ public class CameraMoving : MonoBehaviour
 
     private void Update()
     {
-        //Vector3 cameraVec = _cameraTransform.transform.forward;
-        //Vector3 targetVec = _deadLine.gameObject.transform.position - _cameraTransform.position;
-
-        //float scalarAngleBetweenCameraAndDeadLine = Vector3.Dot(cameraVec.normalized, targetVec.normalized);
-        //float angleBetweenCameraAndDeadLine = Vector3.Angle(cameraVec, targetVec);
-
-        //Debug.Log($"Camera Forward Vec : {cameraVec}\n Target Vec : {targetVec}");
-        //Debug.Log($"scalarAngleBetweenCameraAndDeadLine : {scalarAngleBetweenCameraAndDeadLine}");
-        //Debug.Log($"angleBetweenCameraAndDeadLine : {angleBetweenCameraAndDeadLine}");
-
         Move();
+    }
+
+    private void LateUpdate()
+    {
+        if (false == _isChasingPlayer)
+        {
+            _horizontalDistance = _player.transform.position.x - _cameraTransform.position.x;
+            _verticalDistance = _player.transform.position.z - _cameraTransform.position.z;
+
+            if (_verticalDistance > 6.5f)
+            {
+                _cameraDirection = ECameraDirections.Forward;
+
+                _isChasingPlayer = true;
+            }
+            else if (_horizontalDistance < -4.5f)
+            {
+                _cameraDirection = ECameraDirections.Left;
+
+                _isChasingPlayer = true;
+            }
+            else if (_horizontalDistance > -1.5f)
+            {
+                _cameraDirection = ECameraDirections.Right;
+
+                _isChasingPlayer = true;
+            }
+            else
+            {
+                _cameraDirection = ECameraDirections.None;
+            }
+
+            MakeBezierPoint();
+            
+            StartCoroutine(ChasingPlayer());
+        }
+    }
+
+    private void MakeBezierPoint()
+    {
+        _bezierStartPoint = _cameraTransform.position;
+
+        switch (_cameraDirection)
+        {
+            case ECameraDirections.Forward:
+                _bezierTempPoint = _bezierStartPoint + _moveToForwardBetweenTwoPoints;
+                _bezierEndPoint = _bezierStartPoint + _nextForwardPoint;
+                break;
+            case ECameraDirections.Left:
+                _bezierTempPoint = _bezierStartPoint + _moveToLeftBetweenTwoPoints;
+                _bezierEndPoint = _bezierStartPoint + _nextLeftPoint;
+                break;
+            case ECameraDirections.Right:
+                _bezierTempPoint = _bezierStartPoint + _moveToRightBetweenTwoPoints;
+                _bezierEndPoint = _bezierStartPoint + _nextRightPoint;
+                break;
+            default:
+                break;
+        }
     }
 
     private void Move()
@@ -35,5 +110,26 @@ public class CameraMoving : MonoBehaviour
         Vector3 moveVec = new Vector3(0f, 0f, _moveSpeed * Time.deltaTime);
 
         _cameraTransform.position += moveVec;
+    }
+
+    private IEnumerator ChasingPlayer()
+    {
+        float elapsedTime = 0f;
+
+        while (_isChasingPlayer)
+        {
+            elapsedTime += Time.deltaTime;
+
+            if (elapsedTime >= _reviseCameraPositionTime)
+            {
+                elapsedTime = _reviseCameraPositionTime;
+
+                _isChasingPlayer = false;
+            }
+
+            _cameraTransform.position = _bezierCurve.OnePointBezierCurve(_bezierStartPoint, _bezierTempPoint, _bezierEndPoint, elapsedTime / _reviseCameraPositionTime);
+
+            yield return null;
+        }
     }
 }
