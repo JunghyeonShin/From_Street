546d194d7091bbfe3c787d55a5d7355413980f7b
Zziroo <tlslrs1994@naver.com>
2022-03-14 12:56:36 +0900

    Refactor : Refactoring Code 클래스명 변경

--- FromStreet/Assets/Scripts/Map/ObstacleRemovalLine.cs
+++ FromStreet/Assets/Scripts/Map/ObstacleRemovalLine.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeadLine : MonoBehaviour
+public class ObstacleRemovalLine : MonoBehaviour
 {
     [SerializeField] private RandomTiles _randomTiles = null;
 
