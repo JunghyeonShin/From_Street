0ae78e16c34f9d38618c4bb72e12853f5e6e1ad9
Zziroo <tlslrs1994@naver.com>
2022-03-16 03:25:06 +0900

    Fix : Fix Fixed Obstacle Position 고정 장애물 위치 생성을 수정

---
+++ FromStreet/Assets/Scripts/Spawn/RememberFixedObstaclePosition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class RememberFixedObstaclePosition : MonoBehaviour,IRememberFixedObatclePosition
+{
+    private int _rememberPosition = 0;
+
+    public int RememberPosition { get { return _rememberPosition; } }
+
+    public void RememberPoint(int remember)
+    {
+        _rememberPosition = remember;
+    }
+}
